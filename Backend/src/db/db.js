9762cdb97568ee66816c72e8bda2c726432a11d2
SKY~CLC <yuviclc83@gmail.com>
2026-05-19 11:19:22 +0000

    Register Api

---
+++ Backend/src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+async function connectDB(){
+    try {
+        mogoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to database");
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+module.exports = connectDB;
